3d971c13c07f10239578414d05e12335520d4f69
charmenpinkard <charmenapinkard@gmail.com>
2018-07-25 19:13:19 +0000

    automatically backed up by learn

--- bake_test.rb
+++ bake_test.rb
@@ -1,5 +1,5 @@
 perfect_10_list = ["almond flour", "gluten freewhole oats", "kosher salt", "baking powder", "baking soda", "Xanthan gum","slivered almonds, lightly toasted", "mini dark chocolate chips", "olive oil", "agave"]
-amount_list = ["2 cups", "1 cup", "1/2 teaspoon", "1/2 teaspoon", "1/4 teaspoon", "1/2 teaspoon", "1/4 cup", "3/4 cup", "1/2 cup" "1/4 cup"]
+amount_list = ["2 cups", "1 cup", "1/2 teaspoon", "1/2 teaspoon", "1/4 teaspoon", "1/2 teaspoon", "1/4 cup", "3/4 cup", "1/2 cup", "1/4 cup"]
 
 perfect_10_hash = {}
 index = 0
