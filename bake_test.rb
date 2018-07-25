e8970a4aff8a2006d1b61c38d7ebd59a7e849fb6
charmenpinkard <charmenapinkard@gmail.com>
2018-07-25 18:59:01 +0000

    automatically backed up by learn

---
+++ bake_test.rb
@@ -0,0 +1,9 @@
+perfect_10_list = ["almond flour", "gluten freewhole oats", "kosher salt", "baking powder", "baking soda", "Xanthan gum","slivered almonds, lightly toasted", "mini dark chocolate chips", "olive oil", "agave"]
+amount_list = [2 cups, 1 cup, 1/2 teaspoon, 1/2 teaspoon, 1/4 tea spoon, 1/2 tea spoon, 1/4 cup, 3/4 cup, 1/2 cup, 1/4 cup]
+perfect_10_hash = {}
+index = 0
+perfect_10_list.each do |each_item|
+  perfect_10_hash[each_item] = amount_list[index]
+  index += 1
+end
+puts perfect_10_hash
